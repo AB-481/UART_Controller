421617d51d37872489f046e8c8488daf0887293f
AB-481 <ananya.balaji.chn@gmail.com>
2026-02-02 19:09:57 +0530

    Create baud_generator.v

---
+++ src/baud_generator.v
@@ -0,0 +1,30 @@
+`timescale 1ns / 1ps
+module baud_generator #(
+    parameter CLK_FREQ = 100_000_000,
+    parameter BAUD = 115200*16
+)
+(
+    input  wire clk,
+    input  wire rst,
+    output reg  baud_tick
+);
+
+    localparam integer BAUD_DIV = CLK_FREQ/BAUD;
+    integer count;
+
+    always @(posedge clk) begin
+        if (rst) begin
+            count <= 0;
+            baud_tick <= 0;
+        end
+        else begin
+            if (count == BAUD_DIV - 1) begin
+                count <= 0;
+                baud_tick <= 1;
+            end
+            else begin
+                count <= count + 1;
+                baud_tick <= 0;
+            end
+        end
+    end
